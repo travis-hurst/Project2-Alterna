c9272032b78bd35e4ade3b90ec8fa2ef3ada13f8
Allison <allisonmeier01@gmail.com>
2023-10-30 14:47:48 -0400

    added some skeleton files and folders

---
+++ Project2/src/components/Achievements.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AchievementsView() {
+/* TODO: almost definitely will need grittier component deTAils than just this */
+
+}
+
+
+export default AchievementsView
