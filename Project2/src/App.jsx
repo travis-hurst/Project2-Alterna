7f61708f769baaf117906b87affb7abaa8d85ffe
TravisH <hurst_travis@yahoo.com>
2023-11-02 08:56:12 -0400

    Updated Homepage background/items

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -5,12 +5,24 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <p>Project 2</p>
-
-      <Achievements />
-
-    </div>
+      <div className="bg">
+          <div className="interactable-container">
+            <div className="game-col-1">
+              <button className="announcement-select">Announcements</button>
+            </div>
+            <div className="game-col-2">
+              <button className="bounty-select">Bounties</button>
+            </div>
+            <div className="game-col-3">
+            <button className="navigate-select">Navigate</button>
+            </div>
+            <div className="game-col-4">
+              <button className="achievement-select">Achievements
+                <Achievements />
+              </button>
+            </div>
+          </div>
+      </div>
   )
 }
 
