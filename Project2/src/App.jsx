86024b6a50d6a757237fbbc421fc6d6e5858e8cc
TravisH <hurst_travis@yahoo.com>
2023-11-02 13:10:41 -0400

    added selectable bounty board

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import Achievements from './components/Achievements'
 import './App.css'
+import Achievements from './components/Achievements'
+import bountyBoard from "./assets/bounty-board.jpg"
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
               <button className="announcement-select">Announcements</button>
             </div>
             <div className="game-col-2">
-              <button className="bounty-select">Bounties</button>
+              <button className="bounty-select"><img src={bountyBoard} alt="..." width="500" height="350"></img></button>
             </div>
             <div className="game-col-3">
             <button className="navigate-select">Navigate</button>
