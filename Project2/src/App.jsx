1d251d912e46df728705ccf6e9d78d9a99c6dbfa
TravisH <hurst_travis@yahoo.com>
2023-11-02 16:29:04 -0400

    Added Announcement Table

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Achievements from './components/Achievements'
 import bountyBoard from "./assets/bounty-board.jpg"
+import announcementTable from "./assets/announcement-table.svg"
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
       <div className="bg">
           <div className="interactable-container">
             <div className="game-col-1">
-              <button className="announcement-select">Announcements</button>
+              <button className="announcement-select"><img src={announcementTable} alt="..." width="500" height="350"></img></button>
             </div>
             <div className="game-col-2">
               <button className="bounty-select"><img src={bountyBoard} alt="..." width="500" height="350"></img></button>
