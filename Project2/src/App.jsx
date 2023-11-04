b323c315fa9e76d52f1cbce9996d1aa296a85a01
TravisH <hurst_travis@yahoo.com>
2023-11-04 12:45:11 -0400

    Added achievements assets

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -4,6 +4,7 @@ import Achievements from './components/Achievements'
 import bountyBoard from './assets/bounty-board.jpg'
 import announcementTable from './assets/announcement-table.svg'
 import navigateDesk from './assets/navigate-desk.svg'
+import achievementShelf from './assets/achievement-shelf.svg'
 
 function App() {
 
@@ -14,15 +15,13 @@ function App() {
               <button className="announcement-select"><img src={announcementTable} alt="..." width="450" height="300"></img></button>
             </div>
             <div className="game-col-2">
-              <button className="bounty-select"><img src={bountyBoard} alt="..." width="550" height="400"></img></button>
+              <button className="bounty-select"><img src={bountyBoard} alt="..." width="550" height="450"></img></button>
             </div>
             <div className="game-col-3">
             <button className="navigate-select"><img src={navigateDesk} alt="..." width="700" height="550"></img></button>
             </div>
             <div className="game-col-4">
-              <button className="achievement-select">Achievements
-                <Achievements />
-              </button>
+              <button className="achievement-select"><img src={achievementShelf} alt="..." width="450" height="300"></img><Achievements /></button>
             </div>
           </div>
       </div>
