29026d9f0917b9733f60f9d1dbbf775fce23f758
TravisH <hurst_travis@yahoo.com>
2023-11-06 17:18:13 -0500

    Percent based images to scale better

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -12,16 +12,16 @@ function App() {
       <div className="bg">
           <div className="interactable-container">
             <div className="game-col-1">
-              <button className="announcement-select"><img src={announcementTable} alt="..." width="450" height="300"></img></button>
+              <button className="announcement-select"><img src={announcementTable} alt="..." width="100%" height="auto" objectFit="contain"></img></button>
             </div>
             <div className="game-col-2">
-              <button className="bounty-select"><img src={bountyBoard} alt="..." width="550" height="450"></img></button>
+              <button className="bounty-select"><img src={bountyBoard} alt="..." width="100%" height="auto" objectFit="contain"></img></button>
             </div>
             <div className="game-col-3">
-            <button className="navigate-select"><img src={navigateDesk} alt="..." width="700" height="550"></img></button>
+            <button className="navigate-select"><img src={navigateDesk} alt="..." width="100%" height="auto" objectFit="contain"></img></button>
             </div>
             <div className="game-col-4">
-              <button className="achievement-select"><img src={achievementShelf} alt="..." width="450" height="300"></img><Achievements /></button>
+              <button className="achievement-select"><img src={achievementShelf} alt="..." width="100%" height="auto" objectFit="contain"></img><Achievements /></button>
             </div>
           </div>
       </div>
